7a0b94e0f86c3f6bce837c0410651796587e3a33
Deni Nasrullah <deninasrullah@gmail.com>
2022-05-25 16:05:47 +0700

    aa

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -1,18 +1,13 @@
 <?php
-
-// configure
-$from = 'mail@deninasrullah.my.id'; // Replace it with Your Hosting Admin email. REQUIRED!
-$sendTo = 'mail@deninasrullah.my.id'; // Replace it with Your email. REQUIRED!
-$subject = 'New message from contact form';
-$fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message'); // array variable name => Text to appear in the email. If you added or deleted a field in the contact form, edit this array.
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
-$errorMessage = 'There was an error while submitting the form. Please try again later';
+// PHP Mailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 // let's do the sending
 
 if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
     //your site secret key
-    $secret = '6LdAqO0fAAAAAA02UBCCCkCBVD1Utk7Svyvr3fE1';
+    $secret = '6LdqmCAUAAAAANONcPUkgVpTSGGqm60cabVMVaON';
     //get verify response data
 
     $c = curl_init('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
@@ -24,27 +19,50 @@ if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'
 
         try
         {
+            require 'PHPMailer/Exception.php';
+            require 'PHPMailer/PHPMailer.php';
+            require 'PHPMailer/SMTP.php';
+            
+            $mail = new PHPMailer(true);
+            
+            //Server settings
+            //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                   // Enable verbose debug output
+            $mail->isSMTP();                                           // Send using SMTP
+            $mail->Host       = 'smtp.example.com';                    // Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                  // Enable SMTP authentication
+            $mail->Username   = 'your-smtp-user@example.com';          // SMTP username
+            $mail->Password   = 'your_password';                       // SMTP password
+            $mail->SMTPSecure = 'ssl';                                 // 'ssl' or 'tls' or Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+            $mail->Port       = 465;                                   // TCP port to connect to. For Example: 25 - Simple SMTP. 465 - SSL SMTP. 587 - TLS SMTP.
+
+            //From
+            $mail->setFrom('your-smtp-user@example.com', 'Mailer');    // Add your hosting account email or server admin email
+            //Recipient
+            $mail->addAddress('yourmail@example.com', 'Joe User');     // Add a recipient (your email). Add your name
+            //ReplyTo
+            $mail->addReplyTo($_POST['email'], $_POST['name']);        // Do not change this line
+
+            // Content
+            $mail->isHTML(true);                                       // Set email format to HTML
+            $mail->Subject = 'New message from contact form';          // Email subject. You can change this text
+            
+            $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
+
             $emailText = nl2br("You have new message from Contact Form\n");
 
             foreach ($_POST as $key => $value) {
-
                 if (isset($fields[$key])) {
                     $emailText .= nl2br("$fields[$key]: $value\n");
                 }
             }
-
-            $headers = array('Content-Type: text/html; charset="UTF-8";',
-                'From: ' . $from,
-                'Reply-To: ' . $from,
-                'Return-Path: ' . $from,
-            );
             
-            mail($sendTo, $subject, $emailText, implode("\n", $headers));
+            $mail->Body    = $emailText;
 
+            $mail->send();
+            $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!'; // You can change this text (message)
             $responseArray = array('type' => 'success', 'message' => $okMessage);
-        }
-        catch (\Exception $e)
-        {
+        } catch (Exception $e) {
+            $errorMessage = "There was an error while submitting the form. Please try again later. Mailer Error: {$mail->ErrorInfo}"; // You can change this text (message)
             $responseArray = array('type' => 'danger', 'message' => $errorMessage);
         }
 
